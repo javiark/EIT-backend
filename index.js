740662931416e6b195fe93e4fd812cef756a9d70
Javier <javier778@hotmail.com>
2023-03-16 18:46:31 -0300

    añadir listen port

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hola node")
+const { ok } = require("assert");
+const express = require("express")
+const app = express();
+const port =4000;
+
+app.get('/',(request, response)=>{
+    response.send({
+        msg:`Bienvenido a mi servidor express ${port}`,
+        ok:true
+    })
+})
+
+// GET para traer productos
+// POST para mandar datos
+//PUT o PATCH una modifica toda y otra otra parte. Modifica datos de un producto
+//DELETE puedo borrar un producto
+
+app.listen(port, ()=>{
+    console.log(`Servidor funcionando en puerto ${port}`)
+})
+
+//hola
